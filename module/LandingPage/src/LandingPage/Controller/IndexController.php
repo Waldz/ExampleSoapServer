349415f33388488d8ffb223a92ae2a2a4cc96876
Valdas Petrulis <petrulis.valdas@gmail.com>
2014-05-05 15:56:17 +0300

    * UI form for Word SOAP service demonstration

--- module/LandingPage/src/LandingPage/Controller/IndexController.php
+++ module/LandingPage/src/LandingPage/Controller/IndexController.php
@@ -10,17 +10,67 @@
 namespace LandingPage\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Soap\Client;
+use Zend\View\Helper\ServerUrl;
 use Zend\View\Model\ViewModel;
 use Zend\Stdlib\RequestInterface;
 use Zend\Stdlib\ResponseInterface;
 
 class IndexController extends AbstractActionController
-{   
-    
+{
+
+    /** @var Client */
+    private $_soapClient;
+
+    /**
+     * @param Client $soapClient
+     */
+    public function setSoapClient($soapClient)
+    {
+        $this->_soapClient = $soapClient;
+    }
+
+    /**
+     * @return Client
+     */
+    public function getSoapClient()
+    {
+        if(!isset($this->_soapClient)) {
+            $helperServer = new ServerUrl();
+            $this->_soapClient = new Client(
+                $helperServer($this->url()->fromRoute('word-soap'))
+            );
+        }
+        return $this->_soapClient;
+    }
+
+    /**
+     * UI form for Word SOAP service demonstration
+     *
+     * @return array|ViewModel
+     */
     public function indexAction()
-    {           
-        return new ViewModel(array(
-        ));
+    {
+        $soapClient = $this->getSoapClient();
+        $view = new ViewModel();
+
+        // Read form params
+        $word = $this->params()->fromQuery('word');
+        $view->setVariable('word', $word);
+
+        // Perform request if form was submitted
+        if ($this->params()->fromQuery('submit')) {
+            try {
+                $wordFlipped = $soapClient->wordFlip($word);
+                $view->setVariable('wordFlipped', $wordFlipped);
+            } catch(\Exception $e) {
+                $view->setVariable('wordFlipped', null);
+            }
+            $view->setVariable('soapRequest', $soapClient->getLastRequest());
+            $view->setVariable('soapResponse', $soapClient->getLastResponse());
+        }
+
+        return $view;
     }
 
     public function pageAction()
